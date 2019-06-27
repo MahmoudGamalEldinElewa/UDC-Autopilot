0f25e73297a0ad45dfb0341e9f2affaec04dbb0d
Mahmoud Gamal Eldin Elewa <38992224+MahmoudGamalEldinElewa@users.noreply.github.com>
2019-06-27 14:11:21 +0200

    Renumbering, Partitioning and solution sections replaced with function.

--- Truss.m
+++ Truss.m
@@ -46,23 +46,10 @@ end
 
 [Ac,AE,Re,ndpos,Aml] = dewany(Nn,Nm,M_Load,N_Load,Connection,L,RL,RT);
 
-%% Renumbering and Partitioning
-Sjre(Re,Re)=Sj;
-S=Sjre(1:ndpos,1:ndpos);
-Sdr=Sjre(1:ndpos,ndpos+1:2*Nn);
-Srd=transpose(Sdr);
-Srr=Sjre(ndpos+1:2*Nn,ndpos+1:2*Nn);
-Acre(Re,1)=Ac;
-Ad=Acre(1:ndpos);
-Arl=-Acre(ndpos+1:2*Nn);
-Djre(Re,1)=Dj;
-Dr=Djre(ndpos+1:2*Nn);
-%% Solution
-D=S\(Ad-Sdr*Dr);
-Djre=[D;Dr];
-Dj=Djre(Re)
-Ard=Srd*D+Srr*Dr;
-AR=Arl+Ard
+%% Renumbering, Partitioning, Solving
+
+[Dj,AR] = Renum_Part_Solve(Sj,Re,ndpos,Nn,Ac,Dj);
+
 %% Plotting Deformation
 DSF=100;
 figure
