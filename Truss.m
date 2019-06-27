8cd1069f0cb6973ec9d18d370592e6816f5f5851
Mahmoud Gamal Eldin Elewa <38992224+MahmoudGamalEldinElewa@users.noreply.github.com>
2019-06-27 20:36:41 +0200

    Optimizing Code for readability

--- Truss.m
+++ Truss.m
@@ -1,6 +1,8 @@
 clc
 clearvars
 close all
+
+
 %% Inputs
 P_1= 1e3; P_2= 2e3;
 E=[7.3084e10];
@@ -12,19 +14,23 @@ Connection=[1,2;2,3;3,4;5,6;6,7;7,8;1,5;2,6;3,7;4,8;1,6;2,7;4,7];
 Constr=[0,0;NaN,NaN;NaN,NaN;NaN,0;NaN,NaN;NaN,NaN;NaN,NaN;NaN,NaN];
 N_Load=[5,P_1,-P_1;6,0,-P_1;7,0,-P_2;8,0,-P_1];
 M_Load=[1,0,0,0,0,0];
+
+
 %% Get Dj, RL, Local and Global Stiffness Matrices
-
 [Nm,Nn,Dj,RL,RT,L,Sj,Smd] = Dj_RL_StiffnessMat(M_Info,Nodes_Coordinates,Constr,Connection,E,Area);
-%% Get Fixed End Actions, Equivalent loads,Combined loads and  Renumbering DOF and Get number of possible DOF
 
+
+%% Get Fixed End Actions, Equivalent loads,Combined loads and  Renumbering DOF and Get number of possible DOF
 [Ac,AE,Re,ndpos,Aml] = Aml_AE_Ac_Renum(Nn,Nm,M_Load,N_Load,Connection,L,RL,RT);
 
-%% Renumbering, Partitioning, Solving
 
+%% Renumbering, Partitioning, Solving
 [Dj,AR] = Renum_Part_Solve(Sj,Re,ndpos,Nn,Ac,Dj);
+
 
 %% Plotting Deformation
 plot_deformation(Connection,Dj,Nodes_Coordinates,Nm)
+
 
 %% Plotting Diagrams
 Am=plot_diagrams(Connection,Dj,Nm,M_Load,Aml,RT,Smd,L);
