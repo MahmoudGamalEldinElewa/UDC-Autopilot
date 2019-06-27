c3010f86cbd8d084da55b58ab30887cfdf814ea3
Mahmoud Gamal Eldin Elewa <38992224+MahmoudGamalEldinElewa@users.noreply.github.com>
2019-06-27 14:25:40 +0200

    Added comments to explain the code

--- Renum_Part_Solve.m
+++ Renum_Part_Solve.m
@@ -1,21 +1,21 @@
 function [Dj,AR] = Renum_Part_Solve(Sj,Re,ndpos,Nn,Ac,Dj)
 %% Renumbering and Partitioning
-Sjre(Re,Re)=Sj;
-S=Sjre(1:ndpos,1:ndpos);
-Sdr=Sjre(1:ndpos,ndpos+1:2*Nn);
-Srd=transpose(Sdr);
-Srr=Sjre(ndpos+1:2*Nn,ndpos+1:2*Nn);
-Acre(Re,1)=Ac;
-Ad=Acre(1:ndpos);
-Arl=-Acre(ndpos+1:2*Nn);
-Djre(Re,1)=Dj;
-Dr=Djre(ndpos+1:2*Nn);
+Sjre(Re,Re)=Sj;                         % Renumbered Joint Stiffness Matrix
+S=Sjre(1:ndpos,1:ndpos);                % Get S
+Sdr=Sjre(1:ndpos,ndpos+1:2*Nn);         % Get Sdr
+Srd=transpose(Sdr);                     % Get Srd
+Srr=Sjre(ndpos+1:2*Nn,ndpos+1:2*Nn);    % Get Srr
+Acre(Re,1)=Ac;                          % Renumber Ac
+Ad=Acre(1:ndpos);                       % get Ad
+Arl=-Acre(ndpos+1:2*Nn);                % get Arl
+Djre(Re,1)=Dj;                          % Renumber displacements vector
+Dr=Djre(ndpos+1:2*Nn);                  % restrained displacements vector
 %% Solution
-D=S\(Ad-Sdr*Dr);
-Djre=[D;Dr];
-Dj=Djre(Re)
-Ard=Srd*D+Srr*Dr;
-AR=Arl+Ard
+D=S\(Ad-Sdr*Dr);                        % Get possible displacments
+Djre=[D;Dr];                            % Structure total displacements renumbered
+Dj=Djre(Re)                             % Structure total displacements arbitrary
+Ard=Srd*D+Srr*Dr;                       % Get Ard
+AR=Arl+Ard                              % Get total structure reactions
 
 end
 
